40fbaab06916a8b0a1fce0f4df1cb4425a8c5db8
vienhoanglong <Vienhoanglong789@gmail.com>
2022-01-06 00:44:48 +0700

    AbstractFactory Pattern

---
+++ QuanLiRapPhim/GUI/Patterns/AbstractFactory/DatabaseFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiRapPhim.Patterns.AbstracFactory
+{
+    interface DatabaseFactory
+    {
+        DbConnection createConnection();
+        DbCommand createCommand(String sql);
+        DbCommand createCommand(String sql, DbConnection cn);
+        DbDataAdapter createDataAdapter(DbCommand selectCmd);
+        DbParameter createParam(String key, Object value);
+        DbDataReader createDataReader(DbCommand cmd);
+    }
+}
---
+++ QuanLiRapPhim/GUI/Patterns/AbstractFactory/DatabaseMySql.cs
@@ -0,0 +1,57 @@
+﻿using QuanLiRapPhim.Patterns.AbstracFactory;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiRapPhim.Patterns.AbstractFactory
+{
+    class DatabaseMySql : DatabaseFactory
+    {
+        private static readonly object padlock = new object();
+        private SqlConnection con;
+
+
+        //Signleton
+        public DbConnection createConnection()
+        {
+            if (con == null)
+            {
+                //lock == synchronized
+                lock (padlock)
+                {
+                    con = new SqlConnection(@"Data Source=.;Initial Catalog=QLRL;Integrated Security=True");
+                }
+            }
+            return con;
+        }
+
+        public DbCommand createCommand(string sql)
+        {
+            return new SqlCommand(sql, con);
+        }
+
+        public DbParameter createParam(string key, object value)
+        {
+            return new SqlParameter(key, value);
+        }
+
+        public DbDataReader createDataReader(DbCommand cmd)
+        {
+            return cmd.ExecuteReader();
+        }
+
+        public DbCommand createCommand(string sql, DbConnection cn)
+        {
+            return new SqlCommand(sql, (SqlConnection)cn);
+        }
+
+        public DbDataAdapter createDataAdapter(DbCommand selectCmd)
+        {
+            return new SqlDataAdapter((SqlCommand)selectCmd);
+        }
+    }
+}
